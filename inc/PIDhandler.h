ada5c452d8c01e5cbd1c124abc731d5a3fed6375
chunkitmax <chunkitmax1125@gmail.com>
2015-02-08 16:56:30 +0800

    Add: PIDHandler

---
+++ inc/PIDhandler.h
@@ -0,0 +1,34 @@
+/*
+ * PID.h
+ *
+ *  Created on: Jan 15, 2015
+ *      Author: Peter
+ */
+
+#include <cstring>
+#include <libsc/k60/system.h>
+
+#ifndef INC_PID_H_
+#define INC_PID_H_
+
+class PIDhandler
+{
+public:
+
+	explicit PIDhandler(int32_t ref, float kp, float ki, float kd);
+	~PIDhandler();
+	float updatePID(float val, uint16_t dt);
+	void reset();
+
+private:
+
+	float reference;
+	float Kp;
+	float Ki;
+	float Kd;
+	float eSum;
+	float lastError;
+};
+
+
+#endif /* INC_PID_H_ */
---
+++ src/PIDhandler.cpp
@@ -0,0 +1,37 @@
+/*
+ * PIDhandler.cpp
+ *
+ *  Created on: Jan 15, 2015
+ *      Author: Peter
+ */
+
+#include "PIDhandler.h"
+
+using namespace libsc::k60;
+
+PIDhandler::PIDhandler(int32_t ref, float kp, float ki, float kd)
+:
+	reference(ref),
+	Kp(kp),
+	Ki(ki),
+	Kd(kd),
+	eSum(0),
+	lastError(0)
+{
+	System::Init();
+}
+
+void PIDhandler::reset()
+{
+	eSum = 0;
+}
+
+float PIDhandler::updatePID(float val, uint16_t dt)
+{
+	float error = reference - val;
+	float de = (error - lastError) / dt;
+	eSum += error;
+	lastError = error;
+
+	return (float)(Kp * error + Ki * dt + Kd * de);
+}
