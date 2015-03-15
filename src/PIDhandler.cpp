6a0d7a9be533a71745c5a2f5db7ffa10758750f6
jadewu <jadewunue@gmail.com>
2015-03-16 00:54:41 +0800

    modification
    modification

--- src/PIDhandler.cpp
+++ src/PIDhandler.cpp
@@ -6,14 +6,11 @@
  */
 
 #include "PIDhandler.h"
-
 #include <cstring>
-
-#include <libsc/k60/system.h>
 
 using namespace libsc::k60;
 
-PIDhandler::PIDhandler(float *ref, float *kp, float *ki, float *kd)
+PIDhandler::PIDhandler(float *ref, float kp, float ki, float kd)
 :
 	reference(ref),
 	Kp(kp),
@@ -25,33 +22,69 @@ PIDhandler::PIDhandler(float *ref, float *kp, float *ki, float *kd)
 	System::Init();
 }
 
+PIDhandler::PIDhandler(float ref, float kp, float ki, float kd)
+:
+	reference(new float(ref)),
+	Kp(kp),
+	Ki(ki),
+	Kd(kd),
+	eSum(0),
+	epsilon(10),
+	output(0),
+	v(10)
+	lastError(0)
+{
+	System::Init();
+}
+
+void PIDhandler::setKp(float kp)
+{
+	Kp = MAX(0, kp);
+}
+void PIDhandler::setKi(float ki)
+{
+	Ki = MAX(0, ki);
+}
+void PIDhandler::setKd(float kd)
+{
+	Kd = MAX(0, kd);
+}
+
 float PIDhandler::getKp(void)
 {
-	return *Kp;
+	return Kp;
 }
 
 float PIDhandler::getKi(void)
 {
-	return *Ki;
+	return Ki;
 }
 
 float PIDhandler::getKd(void)
 {
-	return *Kd;
+	return Kd;
 }
 
 void PIDhandler::reset()
 {
 	eSum = 0;
-	lastError = 0;
 }
 
-float PIDhandler::updatePID(float val, Timer::TimerInt dt)
+float PIDhandler::updatePID(float val, uint32_t dt)
 {
 	float error = *reference - val;
 	float dE = (error - lastError) / dt;
-	eSum += error * dt;
-	lastError = error;
+	if (abs(error)>epsilon){
+		eSum += error*dt;
+	}
+	if (abs(error)>=v){
+		output = abs(Kp * error) + Ki * esum + Kd * de;
+	}
+	else{
+		output = Kp * error + Ki * esum + Kd * de;
+	}
+	if (output>reference)
+		output = reference;
+	return (float)(output);
 
-	return (float)(*Kp * error + *Ki * dE + *Kd * eSum);
 }
